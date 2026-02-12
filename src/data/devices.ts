55fa4a2ca5e5584812b49367183c7c304b5bfe07
xjhjtzAC <xjhjtzAC@noreply.gitcode.com>
2026-02-12 22:29:32 +0800

    📝 更新博客

--- src/data/devices.ts
+++ src/data/devices.ts
@@ -19,13 +19,13 @@ export const devicesData: DeviceCategory = {
 	PC: [
 		{
 			name: "联想小新Pro 14 2024",
-			image: "/images/device/xiaoxinpro14.jpg",
+			image: "/images/device/xiaoxinpro14.webp",
 			specs: "Gray / 24G + 1TB",
 			description: "搭载AMD Ryzen7 8745H处理器与杜比音效，配备100W快充",
 			link: "https://www.bilibili.com/video/BV1um411S7U4",
 		},
 	],
-	drone: [
+	Drone: [
 		{
 			name: "DJI Mini2",
 			image: "/images/device/mini2.jpg",
@@ -33,6 +33,16 @@ export const devicesData: DeviceCategory = {
 			description:
 				"搭载1/2.3英寸CMOS传感器，支持4K视频拍摄和31分钟飞行时间",
 			link: "https://store.dji.com/cn/product/mini-2",
+		},
+	],
+	Osmo: [
+		{
+			name: "DJI Osmo pocket 3",
+			image: "/images/device/pocket.webp",
+			specs: "标准套装",
+			description:
+				"搭载1英寸CMOS传感器，支持4K60FPS视频拍摄，配备三轴机械云台",
+			link: "https://www.dji.com/cn/osmo-pocket-3",
 		},
 	],
 };
