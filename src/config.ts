528d3929736456efc34a29b8b969781cfe513d4b
xjhjtzAC <xjhjtzAC@noreply.gitcode.com>
2026-04-04 19:31:39 +0800

    更新博客

--- src/config.ts
+++ src/config.ts
@@ -480,7 +480,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 export const footerConfig: FooterConfig = {
 	enable: true, // 是否启用Footer HTML注入功能
 	customHtml:
-		"“你是秋风里落进我心底的一片叶，从此我的四季，都带着未说出口的秋意。” | 本博客使用的图片素材均已获得授权", // HTML格式的自定义页脚信息，例如备案号等，默认留空
+		"“你是秋风里落进我心底的一片叶，从此我的四季，都带着未说出口的秋意。”", // HTML格式的自定义页脚信息，例如备案号等，默认留空
 	// 也可以直接编辑 FooterConfig.html 文件来添加备案号等自定义内容
 	// 注意：若 customHtml 不为空，则使用 customHtml 中的内容；若 customHtml 留空，则使用 FooterConfig.html 文件中的内容
 	// FooterConfig.html 可能会在未来的某个版本弃用
