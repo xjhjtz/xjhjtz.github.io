02593aa2f0725607864328f9bea9cba6428001ea
xjhjtz <xjhjtzac@163.com>
2026-07-01 22:17:44 +0800

    添加评论系统

--- src/config.ts
+++ src/config.ts
@@ -427,17 +427,17 @@ export const expressiveCodeConfig: ExpressiveCodeConfig = {
 };
 
 export const commentConfig: CommentConfig = {
-	enable: false, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
-	system: "twikoo", // 评论系统选择: "twikoo" | "giscus"
+	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
+	system: "giscus", // 评论系统选择: "twikoo" | "giscus"
 	twikoo: {
 		envId: "https://twikoo.vercel.app",
 		lang: SITE_LANG,
 	},
 	giscus: {
-		repo: "your-github-username/your-repo-name",
-		repoId: "your-repo-id",
+		repo: "xjhjtz/xjhjtz.github.io",
+		repoId: "R_kgDORObyEA",
 		category: "Announcements",
-		categoryId: "your-category-id",
+		categoryId: "DIC_kwDORObyEM4DASfZ",
 		mapping: "pathname",
 		strict: "0",
 		reactionsEnabled: "1",
